d8aa337f0d20ccf5b685139aa32542f0b1775ea8
bogdan.cret@spamexperts.com <bogdan.cret@spamexperts.com>
2016-02-26 12:04:03 +0200

    Refs #24706. Use absolute path when getting version. (cpanel)

--- bin/uninstall.php
+++ bin/uninstall.php
@@ -351,7 +351,7 @@ class Uninstaller
 	 */
 	private function start_uninstall()
 	{
-		$version = file_get_contents(".." . DS . "application" . DS . "version.txt");
+		$version = file_get_contents(dirname(__FILE__)."/.." . DS . "application" . DS . "version.txt");
 		echo "This system will uninstall ProSpamFilter v{$version}\n";
 		return true;
 	}
