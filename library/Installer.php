116e5cc530cc73165990cfb74271404a0855ee2c
bogdan.cret@spamexperts.com <bogdan.c@spamexperts.com>
2016-04-19 16:07:42 +0300

    Refs #25450. Cpanel uninstaller script updated

---
+++ library/Installer.php
@@ -0,0 +1,290 @@
+<?php
+
+class Uninstaller
+{
+    /**
+     * @var AbstractFilesystem
+     */
+    protected $filesystem;
+
+    /**
+     * @var Installer_InstallPaths
+     */
+    protected $paths;
+
+    /**
+     * @var SpamFilter_PanelSupport_Cpanel
+     */
+    protected $panelSupport;
+
+    /**
+     * @var SpamFilter_Logger
+     */
+    protected $logger;
+
+    /**
+     * @var string
+     */
+    protected $currentVersion;
+
+    /**
+     * @var Output_OutputInterface
+     */
+    protected $output;
+
+    /**
+     * @var bool
+     */
+    protected $resetMx;
+
+    public function __construct(Installer_InstallPaths $paths, Filesystem_AbstractFilesystem $filesystem, Output_OutputInterface $output, $resetMx = false)
+    {
+        $this->output = $output;
+        $this->filesystem = $filesystem;
+        $this->paths = $paths;
+        $this->logger = Zend_Registry::get('logger');
+        $this->resetMx = $resetMx;
+    }
+
+    public function uninstall()
+    {
+        try {
+            $this->doUninstall();
+        } catch (Exception $exception) {
+            $this->output->error($exception->getMessage());
+            $this->logger->debug($exception->getMessage());
+            $this->logger->debug($exception->getTraceAsString());
+        }
+    }
+
+    private function doUninstall()
+    {
+        $this->outputStartMessage();
+        $this->checkRequirementsAreMet();
+        $this->resetMXs();
+        $this->removeAddonFromCpanel();
+        $this->removeHooks();
+        $this->filesystem->removeDirectory($this->paths->config);
+        $this->filesystem->removeDirectory($this->paths->destination);
+        $this->removeUpdateCronjob();
+        $this->output->write("_n_n***** We're sad to see you go, but ProSpamFilter has now been uninstalled from your system! *****_n_n");
+    }
+
+    private function outputStartMessage()
+    {
+        $this->output->write("*** Uninstallation Process ***");
+        $this->output->write("This application will UNINSTALL your ProSpamFilter with full configuration.");
+        $this->output->write('');
+
+        $file = __DIR__.'/../application/version.txt';
+        $version = file_get_contents($file);
+        $this->output->write("This system will uninstall ProSpamFilter v{$version}");
+    }
+
+    private function checkRequirementsAreMet()
+    {
+        if(! file_exists($this->paths->destination)) {
+            throw new Exception("ProSpamFilter is not installed");
+        }
+
+        $this->checkUserIsRoot();
+
+        if (! is_cli()) {
+            throw new Exception("This program can only be ran from CLI");
+        }
+
+        // Additional check for resetMX action.
+        if($this->resetMx){
+            if(! class_exists("SpamFilter_Hooks") || ! class_exists("SpamFilter_PanelSupport_Cpanel")){
+                throw new Exception("ERROR: Required files are missing! Uninstaller cannot reset mx records!");
+            }
+
+            if(! function_exists("mb_strtolower")){
+                throw new Exception("Multibyte Extension is required to proceed this action! Please install Multibyte Extenstion.");
+            }
+        }
+    }
+
+    private function resetMXs()
+    {
+        if (! $this->resetMx) {
+            return;
+        }
+
+        $this->output->info("Resetting domains MX records. Please wait... ");
+        $hooks = new SpamFilter_Hooks;
+        $panel = new SpamFilter_PanelSupport_Cpanel();
+        $failures = array();
+
+        $domains = $panel->getDomains(array('username' => SpamFilter_Core::getUsername(), 'level' => 'owner'));
+
+        if(empty($domains)){
+            $this->output->ok("There are no domains. Skipping this step.");
+            return;
+        }
+
+        foreach ($domains as $domain){
+            $this->output->info("Resetting: " . $domain['domain']);
+            $result = $hooks->DelDomain(trim($domain['domain']), true, true);
+
+            //If domain isn't added to SpamFilter then it isn't error
+            if($result['status'] != 1 && $result['reason'] != 'NO_SUCH_DOMAIN'){
+                $failures[] = array('domain' => $domain['domain'], 'reason' => $result['reason']);
+            }
+        }
+
+        if(empty($failures)) {
+            $this->output->ok("MXs records was successfully reset!");
+        } else {
+            $this->output->error("MX records reset failed for domains:");
+
+            foreach($failures as $fail){
+                $this->output->write("  " . $fail['domain'] . " - " . $fail['reason']);
+            }
+        }
+
+        $this->output->ok("Done");
+    }
+
+    private function checkUserIsRoot()
+    {
+        $whoami = shell_exec('whoami');
+        $whoami = trim($whoami);
+        if ($whoami != "root") {
+            $this->output->error("This can only be installed by 'root' (not: '$whoami').");
+            exit(1);
+        }
+    }
+
+    private function removeAddonFromCpanel()
+    {
+        // Delink cPanel addon from webdirs.
+        $webDirs = [
+            "/usr/local/cpanel/base/frontend/x3/prospamfilter/",
+            "/usr/local/cpanel/base/frontend/x3mail/prospamfilter/",
+            "/usr/local/cpanel/whostmgr/docroot/cgi/psf/",
+            "/usr/local/prospamfilter/frontend/cpanel/psf",
+        ];
+
+        foreach ($webDirs as $dir) {
+            $this->output->info("Unlinking $dir");
+            @unlink($dir);
+        }
+
+        // Delink WHM addon to CGI dir.
+        @unlink("/usr/local/cpanel/whostmgr/docroot/cgi/addon_prospamfilter.php");
+        @unlink("/usr/local/cpanel/whostmgr/docroot/cgi/addon_prospamfilter.cgi");
+
+        // Delink WHM icon
+        @unlink("/usr/local/cpanel/whostmgr/docroot/themes/x/icons/prospamfilter.gif");
+
+        // Unregister cPanel addon (makes icon sprites)
+        system("/usr/local/cpanel/bin/unregister_cpanelplugin /usr/local/prospamfilter/frontend/cpanel/cpanel11/prospamfilter.cpanelplugin");
+
+        // Removes DynamicUI for paper_lantern theme
+        @unlink("/usr/local/cpanel/base/frontend/paper_lantern/dynamicui/dynamicui_psf.conf");
+
+        // Refresh cache (cPanel Bug: #1049678)
+        system("touch /usr/local/cpanel/base/frontend/x3/dynamicui.conf");
+        system("touch /usr/local/cpanel/base/frontend/x3mail/dynamicui.conf");
+    }
+
+    private function removeHooks()
+    {
+        $panel = new SpamFilter_PanelSupport_Cpanel();
+        $hooks = array('file' => DEST_PATH . '/bin/hook.php',
+            'do' => 'delete',
+            'hooks' => array(
+                array('category' => 'Whostmgr',
+                    'event' => 'Accounts::Create',
+                    'stage' => 'post',
+                    'action' => ''),
+
+                array('category' => 'Whostmgr',
+                    'event' => 'Accounts::Remove',
+                    'stage' => 'pre',
+                    'action' => ''),
+
+                array('category' => 'Whostmgr',
+                    'event' => 'Accounts::Modify',
+                    'stage' => 'post',
+                    'action' => ''),
+
+                array('category' => 'PkgAcct',
+                    'event' => 'Restore',
+                    'stage' => 'post',
+                    'action' => ''),
+
+                array('category' => 'Cpanel',
+                    'event' => 'Api1::Park::park',
+                    'stage' => 'post',
+                    'action' => ''),
+
+                array('category' => 'Cpanel',
+                    'event' => 'Api1::Park::unpark',
+                    'stage' => 'pre',
+                    'action' => ''),
+
+                array('category' => 'Cpanel',
+                    'event' => 'Api2::AddonDomain::addaddondomain',
+                    'stage' => 'post',
+                    'action' => ''),
+
+                array('category' => 'Cpanel',
+                    'event' => 'Api2::AddonDomain::deladdondomain',
+                    'stage' => 'pre',
+                    'action' => ''),
+
+                array('category' => 'Cpanel',
+                    'event' => 'Api1::SubDomain::addsubdomain',
+                    'stage' => 'post',
+                    'action' => ''),
+
+                array('category' => 'Cpanel',
+                    'event' => 'Api2::SubDomain::addsubdomain',
+                    'stage' => 'post',
+                    'action' => ''),
+
+                array('category' => 'Cpanel',
+                    'event' => 'Api2::SubDomain::delsubdomain',
+                    'stage' => 'pre',
+                    'action' => ''),
+
+                array('category' => 'Cpanel',
+                    'event' => 'Api2::CustInfo::savecontactinfo',
+                    'stage' => 'post',
+                    'action' => ''),
+
+                array('category' => 'Cpanel',
+                    'event' => 'Api2::Email::setmxcheck',
+                    'stage' => 'post',
+                    'action' => ''),
+
+                array('category' => 'Cpanel',
+                    'event' => 'Api2::Email::setalwaysaccept',
+                    'stage' => 'post',
+                    'action' => '')
+            )
+        );
+
+        $panel->manageHooks($hooks);
+    }
+
+    private function removeUpdateCronjob()
+    {
+        $this->output->info("Removing cronjob...");
+        @unlink("/etc/cron.d/prospamfilter");
+        $this->output->ok("Done");
+
+        $this->output->info("Reloading cron...");
+        $cron = touch("/etc/crontab"); //Ha! Sneaky way of reloading which is univeral at CentOS/Debian :-)
+        if ($cron) {
+            $this->output->ok("Done");
+        } else {
+            $this->output->error("FAILED!");
+            $this->output->error("** Unable to reload cron **");
+            $this->output->error("--> Please rotate the cron daemon to make sure the cronjob is not longer being executed.");
+            sleep(10);
+        }
+    }
+}
