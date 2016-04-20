795c3b39a00d6a6f7cf1c52d55ab0d26289acee9
bogdan.cret@spamexperts.com <bogdan.c@spamexperts.com>
2016-04-20 14:32:30 +0300

    Refs #25450. Use `shell_exec` instead of `system`

--- library/Filesystem/LinuxFilesystem.php
+++ library/Filesystem/LinuxFilesystem.php
@@ -4,13 +4,11 @@ class Filesystem_LinuxFilesystem extends Filesystem_AbstractFilesystem
 {
     public function removeDirectory($directory)
     {
-        system("rm -rf $directory");
+        return shell_exec("rm -rf $directory");
     }
 
     public function symlinkDirectory($target, $link)
     {
-        system("ln -sf $target $link", $return);
-
-        return $return;
+        return shell_exec("ln -sf $target $link");
     }
 }
--- library/Filesystem/WindowsFilesystem.php
+++ library/Filesystem/WindowsFilesystem.php
@@ -4,14 +4,13 @@ class Filesystem_WindowsFilesystem extends Filesystem_AbstractFilesystem
 {
     public function removeDirectory($directory)
     {
-        system("rmdir $directory /s /q");
+        return shell_exec("rmdir \"$directory\" /s /q");
     }
 
     public function symlinkDirectory($target, $link)
     {
         $command = "MKLINK " . '"' . $link . '" "' . $target . '" /D';
-        system($command, $return);
 
-        return $return;
+        return shell_exec($command);
     }
 }
