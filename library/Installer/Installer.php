6541c36f4ae7ebdc36f74c033808b19483fd792e
Claudia Petre <claudia@spamexperts.com>
2017-07-31 10:39:27 +0200

    Refs #18 Update Jquery and add it to cPanel if doesn't exists

--- library/Installer/Installer.php
+++ library/Installer/Installer.php
@@ -105,6 +105,11 @@ class Installer_Installer
 
         file_put_contents('/usr/local/cpanel/whostmgr/addonfeatures/prospamfilter', 'prospamfilter:SpamExperts');
 
+        if (!file_exists("/usr/local/cpanel/base/unprotected/libraries/jquery/3.2.0/")) {
+            mkdir("/usr/local/cpanel/base/unprotected/libraries/jquery/3.2.0/");
+            copy("/usr/local/prospamfilter/public/js/jquery.min.js","/usr/local/cpanel/base/unprotected/libraries/jquery/3.2.0/jquery-3.2.0.min.js");
+        }
+
         $this->setUpdateCronjob();
         $this->setupSuidPermissions();
         $this->removeMigrationFiles($this->currentVersion);
